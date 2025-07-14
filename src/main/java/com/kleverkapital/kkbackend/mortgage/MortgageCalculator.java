f8162dc2b12ea5ec9d567f84f6a4ded6252e01e0
Karthik Ramaraju <karthikeyaramaraju@gmail.com>
2025-07-13 20:35:03 -0500

    b

--- src/main/java/com/kleverkapital/kkbackend/mortgage/MortgageCalculator.java
+++ src/main/java/com/kleverkapital/kkbackend/mortgage/MortgageCalculator.java
@@ -1,10 +1,15 @@
 package com.kleverkapital.kkbackend.mortgage;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class MortgageCalculator {
 
     public static final int PAYMENTS_PER_YEAR = 12;
